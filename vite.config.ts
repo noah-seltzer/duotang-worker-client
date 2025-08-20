8a3729fb62d2d34c4356892cc08ce5726b5eeb64
Noah Seltzer <noahseltzer@outlook.com>
2025-08-20 13:03:22 -0700

    feat(client):add sentry with source maps

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,26 @@ import react from '@vitejs/plugin-react-swc'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        tanstackRouter({ target: 'react', autoCodeSplitting: true, routesDirectory: './src/react-app/routes'}),
-        cloudflare()
-    ]
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+            routesDirectory: './src/react-app/routes'
+        }),
+        cloudflare(),
+        sentryVitePlugin({
+            org: 'noah-seltzer',
+            project: 'duotang-worker-client',
+            authToken: process.env.SENTRY_AUTH_TOKEN
+        })
+    ],
+
+    build: {
+        sourcemap: true
+    }
 })
