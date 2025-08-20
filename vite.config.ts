989ba425e82b245b1efb1374351d96c990779cfa
Noah Seltzer <noahseltzer@outlook.com>
2025-08-20 12:16:04 -0700

    fix(client):fix tanstack config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+        tanstackRouter({ target: 'react', autoCodeSplitting: true, routesDirectory: './src/react-app/routes' }),
         cloudflare()
     ]
 })
